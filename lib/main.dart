840b941afe4c84187b2c8d50f2d2b483076cb118
Dekpfastatt <driss.elkhalouki@posteo.net>
2021-10-19 18:46:43 +0200

    Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:coachtennis/sample.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowCheckedModeBanner: false,
       title: 'Coach Tennis',
       theme: ThemeData(
         primarySwatch: Colors.green,
@@ -27,13 +31,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final introkey = GlobalKey<IntroductionScreenState>();
+
+  Widget _buildFullscrenImage() {
+    return Image.asset(
+      'assets/splashscreen1.jpg',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => HomePage()),
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 20.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introkey,
+      globalBackgroundColor: Colors.white,
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          child: const Text(
+            'Get Started!',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "Welcome to Tennis coach",
+          body: "Improve your tennis with a coach",
+          image: _buildFullscrenImage(),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "More efficient",
+          body: "Technics are important and make you more efficient",
+          image: _buildImage("splashscreen2.jpg"),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Book hours",
+          body: "Coach are available to make you more powerful. Book hours!",
+          image: _buildImage("splashscreen3.jpg"),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip', style: TextStyle(fontSize: 22.0)),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done',
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22.0)),
+      // curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(10.0, 10.0),
+        activeColor: Colors.green,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tennis Coach"),
+        title: const Text("HomePage"),
       ),
-      body: Center(),
+      body: const Center(child: Text("Main Screen")),
     );
   }
 }
