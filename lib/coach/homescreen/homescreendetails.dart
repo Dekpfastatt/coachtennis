66472f920bfa79c72227ec91791d89c162f855fe
Dekpfastatt <driss.elkhalouki@posteo.net>
2021-12-11 19:43:36 +0100

    Dahsboard 2

--- lib/coach/homescreen/homescreendetails.dart
+++ lib/coach/homescreen/homescreendetails.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coachtennis/coach/homescreen/model/coachschedule.dart';
 import 'package:flutter/material.dart';
 
@@ -9,29 +10,34 @@ class HomeScreenDetails extends StatefulWidget {
 }
 
 class _HomeScreenDetailsState extends State<HomeScreenDetails> {
+  CollectionReference _coachschedule =
+      FirebaseFirestore.instance.collection('coachschedule');
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView.builder(
-        itemCount: CoachSchedule.scheduleList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 12,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                Text(CoachSchedule.scheduleList[index].scheduleDate),
-                Text(CoachSchedule.scheduleList[index].scheduleTimeFrom),
-                Text(CoachSchedule.scheduleList[index].scheduleTimeTo),
-                Text(CoachSchedule.scheduleList[index].studentName),
-                Text(CoachSchedule.scheduleList[index].clubDetails),
-                Text(CoachSchedule.scheduleList[index].clubCourt),
-              ],
-            ),
-          );
-        },
+      child: Scaffold(
+        body: StreamBuilder(
+            stream: _coachschedule.snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+              if (streamSnapshot.hasData) {
+                return ListView.builder(
+                  itemCount: streamSnapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot docsnapshot =
+                        streamSnapshot.data.docs[index];
+                    return Card(
+                      margin: EdgeInsets.all(10.0),
+                      child: ListTile(
+                          title: Text(docsnapshot['clubdetails'].toString()),
+                          subtitle: Text(docsnapshot['timefrom'].toString()),
+                          trailing: Text(docsnapshot['timeto'].toString())),
+                    );
+                  },
+                );
+              }
+              return Center(child: CircularProgressIndicator());
+            }),
       ),
     );
   }
