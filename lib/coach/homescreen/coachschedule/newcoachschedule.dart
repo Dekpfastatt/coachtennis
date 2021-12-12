cf8d862960b6265e5222684bb144abe5ec8e9bc1
Dekpfastatt <driss.elkhalouki@posteo.net>
2021-12-12 10:35:59 +0100

    New Schedule Coach

---
+++ lib/coach/homescreen/coachschedule/newcoachschedule.dart
@@ -0,0 +1,198 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class NewCoachSchedule extends StatefulWidget {
+  const NewCoachSchedule({Key key}) : super(key: key);
+
+  @override
+  _NewCoachScheduleState createState() => _NewCoachScheduleState();
+}
+
+class _NewCoachScheduleState extends State<NewCoachSchedule> {
+  TextEditingController dateController = new TextEditingController();
+  TextEditingController studentController = new TextEditingController();
+  TextEditingController clubController = new TextEditingController();
+  TextEditingController timeFromController = new TextEditingController();
+  TextEditingController timeToController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Coach Schedule",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Add new schedule",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            Expanded(
+              child: Container(
+                  width: 300,
+                  child: TextFormField(
+                    controller: dateController,
+                    decoration: const InputDecoration(
+                        labelText: "Schedule date",
+                        hintText: "Schedule date",
+                        border: OutlineInputBorder()),
+                    autofocus: false,
+                    onTap: () async {
+                      DateTime date = DateTime(2021);
+                      DateFormat formatter = DateFormat('dd/MM/yyyy');
+                      FocusScope.of(context).requestFocus(new FocusNode());
+                      date = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2021),
+                          lastDate: DateTime(2100));
+
+                      dateController.text = formatter.format(date);
+                    },
+                  )),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                width: 300,
+                child: TextFormField(
+                  controller: studentController,
+                  decoration: const InputDecoration(
+                      labelText: "student",
+                      hintText: "student",
+                      border: OutlineInputBorder()),
+                  autofocus: false,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                width: 300,
+                child: TextFormField(
+                  controller: clubController,
+                  decoration: const InputDecoration(
+                      labelText: "club",
+                      hintText: "club",
+                      border: OutlineInputBorder()),
+                  autofocus: false,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                width: 300,
+                child: TextFormField(
+                  controller: timeFromController,
+                  decoration: const InputDecoration(
+                      labelText: "Time from",
+                      hintText: "Time from",
+                      border: OutlineInputBorder()),
+                  autofocus: false,
+                  onTap: () async {
+                    TimeOfDay pickedTime = await showTimePicker(
+                      initialTime: TimeOfDay.now(),
+                      context: context,
+                    );
+
+                    if (pickedTime != null) {
+                      DateTime parsedTime = DateFormat.jm()
+                          .parse(pickedTime.format(context).toString());
+                      String formattedTime =
+                          DateFormat('HH:mm').format(parsedTime);
+                      setState(() {
+                        timeFromController.text = formattedTime;
+                      });
+                    } else {
+                      print("Time is not selected");
+                    }
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                width: 300,
+                child: TextFormField(
+                  controller: timeToController,
+                  decoration: const InputDecoration(
+                      labelText: "Time To",
+                      hintText: "Time To",
+                      border: OutlineInputBorder()),
+                  autofocus: false,
+                  onTap: () async {
+                    TimeOfDay pickedTime = await showTimePicker(
+                      initialTime: TimeOfDay.now(),
+                      context: context,
+                    );
+
+                    if (pickedTime != null) {
+                      DateTime parsedTime = DateFormat.jm()
+                          .parse(pickedTime.format(context).toString());
+                      String formattedTime =
+                          DateFormat('HH:mm').format(parsedTime);
+                      setState(() {
+                        timeToController.text = formattedTime;
+                      });
+                    } else {
+                      print("Time is not selected");
+                    }
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                child: const Text("Add Schedule"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: () {
+                  final String dateSchedule = dateController.text.trim();
+                  final String student = studentController.text.trim();
+                  final String timefrom = timeFromController.text.trim();
+                  final String timeto = timeToController.text.trim();
+                  final String club = clubController.text.trim();
+
+                  final firestoreInstance = FirebaseFirestore.instance;
+
+                  if (dateSchedule.isEmpty &&
+                      student.isEmpty &&
+                      timefrom.isEmpty &&
+                      timeto.isEmpty &&
+                      club.isEmpty) {
+                    print("Missing input");
+                  } else {
+                    var coachschedule = FirebaseAuth.instance.currentUser;
+                    firestoreInstance.collection("coachschedule").add({
+                      "scheduledate": dateSchedule.toString(),
+                      "clubdetails": club.toString(),
+                      "studentname": student.toString(),
+                      "timefrom": timefrom.toString(),
+                      "timeto": timeto.toString(),
+                    });
+                    dateController.text = '';
+                    studentController.text = '';
+                    timeFromController.text = '';
+                    timeToController.text = '';
+                    clubController.text = '';
+
+                    Navigator.of(context).pop();
+                  }
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/coach/homescreen/homescreendetails.dart
+++ lib/coach/homescreen/homescreendetails.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coachtennis/coach/homescreen/model/coachschedule.dart';
 import 'package:flutter/material.dart';
 
+import 'coachschedule/newcoachschedule.dart';
+
 class HomeScreenDetails extends StatefulWidget {
   //const HomeScreenDetails({ Key? key }) : super(key: key);
 
@@ -17,6 +19,15 @@ class _HomeScreenDetailsState extends State<HomeScreenDetails> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => NewCoachSchedule()),
+            );
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.green,
+        ),
         body: StreamBuilder(
             stream: _coachschedule.snapshots(),
             builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
@@ -29,9 +40,35 @@ class _HomeScreenDetailsState extends State<HomeScreenDetails> {
                     return Card(
                       margin: EdgeInsets.all(10.0),
                       child: ListTile(
-                          title: Text(docsnapshot['clubdetails'].toString()),
-                          subtitle: Text(docsnapshot['timefrom'].toString()),
-                          trailing: Text(docsnapshot['timeto'].toString())),
+                          title: Text(docsnapshot['scheduledate'].toString()),
+                          subtitle: Text(docsnapshot['clubdetails'].toString()),
+                          trailing: SizedBox(
+                              width: 200,
+                              child: Column(
+                                children: [
+                                  Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Text(docsnapshot['studentname']
+                                            .toString()),
+                                      ]),
+                                  Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Text('Start:  '),
+                                        Text(
+                                            docsnapshot['timefrom'].toString()),
+                                        Text('  End:  '),
+                                        Text(docsnapshot['timeto'].toString()),
+                                      ]),
+                                ],
+                              ))),
                     );
                   },
                 );
