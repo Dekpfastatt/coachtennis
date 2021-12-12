c14cc400881f77f2d36e110ccff824a8b2954465
Dekpfastatt <driss.elkhalouki@posteo.net>
2021-12-12 11:19:08 +0100

    Edit Schedule Coach

--- lib/coach/homescreen/homescreendetails.dart
+++ lib/coach/homescreen/homescreendetails.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coachtennis/coach/homescreen/model/coachschedule.dart';
 import 'package:flutter/material.dart';
 
+import 'coachschedule/editcoachschedule.dart';
 import 'coachschedule/newcoachschedule.dart';
 
 class HomeScreenDetails extends StatefulWidget {
@@ -14,6 +17,13 @@ class HomeScreenDetails extends StatefulWidget {
 class _HomeScreenDetailsState extends State<HomeScreenDetails> {
   CollectionReference _coachschedule =
       FirebaseFirestore.instance.collection('coachschedule');
+
+  Future<void> _deleteschedule(String coachdetails) async {
+    await _coachschedule.doc(coachdetails).delete();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('You have successfuly deleted!'),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,36 +49,73 @@ class _HomeScreenDetailsState extends State<HomeScreenDetails> {
                         streamSnapshot.data.docs[index];
                     return Card(
                       margin: EdgeInsets.all(10.0),
-                      child: ListTile(
-                          title: Text(docsnapshot['scheduledate'].toString()),
-                          subtitle: Text(docsnapshot['clubdetails'].toString()),
-                          trailing: SizedBox(
-                              width: 200,
-                              child: Column(
-                                children: [
-                                  Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Text(docsnapshot['studentname']
-                                            .toString()),
-                                      ]),
-                                  Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Text('Start:  '),
-                                        Text(
-                                            docsnapshot['timefrom'].toString()),
-                                        Text('  End:  '),
-                                        Text(docsnapshot['timeto'].toString()),
-                                      ]),
-                                ],
-                              ))),
+                      child: Column(
+                        children: [
+                          ListTile(
+                              title: Text(
+                                docsnapshot['scheduledate'].toString(),
+                                style: TextStyle(color: Colors.green),
+                              ),
+                              subtitle:
+                                  Text(docsnapshot['clubdetails'].toString()),
+                              trailing: SizedBox(
+                                  width: 200,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Text(docsnapshot['studentname']
+                                                .toString()),
+                                          ]),
+                                      Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Text('Start:  '),
+                                            Text(
+                                              docsnapshot['timefrom']
+                                                  .toString(),
+                                              style:
+                                                  TextStyle(color: Colors.red),
+                                            ),
+                                            Text('  End:  '),
+                                            Text(
+                                              docsnapshot['timeto'].toString(),
+                                              style:
+                                                  TextStyle(color: Colors.red),
+                                            ),
+                                          ]),
+                                    ],
+                                  ))),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (_) => EditCoachSchedule()),
+                                  );
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  _deleteschedule(docsnapshot.id);
+                                },
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
                     );
                   },
                 );
